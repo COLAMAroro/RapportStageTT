321d0e2e71877d5d1c1ca8ca7ee2aedfd81cda74
Corentin Rondier <corentin.rondier@epitech.eu>
2019-12-18 13:22:22 +0100

    fix build script

--- build.sh
+++ build.sh
@@ -2,4 +2,7 @@
 
 mkdir -p build
 xelatex -synctex=1 --undump=xelatex -shell-escape --output-directory=build projectemplate.tex
+xelatex -synctex=1 --undump=xelatex -shell-escape --output-directory=build projectemplate.tex
+xelatex -synctex=1 --undump=xelatex -shell-escape --output-directory=build projectemplate.tex
+#rerun XeLaTeX 3 times because of possible missing cross-reference
 cp build/projectemplate.pdf internship_report.pdf
