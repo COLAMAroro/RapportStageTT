51f13d0b1a49d5bc97bfffd3cac8cd2bf4ced6b9
Corentin Rondier <corentin.rondier@epitech.eu>
2019-12-20 12:59:26 +0100

    Make char count easy to modify for others

--- true_charcount.sh
+++ true_charcount.sh
@@ -1,1 +1,4 @@
-pdf2txt.py internship_report.pdf | tr -s "[:alumn: ]" | tr -d ".\n" | sed '/^$/d' | wc -c
+file="internship_report.pdf"
+pdf2txt="pdf2txt.py"
+
+$pdf2txt $file | tr -s "[:alumn: ]" | tr -d ".\n" | sed '/^$/d' | wc -c
